8d8ecbc1d24f7df0f4faf0547898a6b9af4a4d37
HarshithaSuresh143 <138377314+HarshithaSuresh143@users.noreply.github.com>
2026-01-12 16:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import generate_data
+import sqlite3
+import pandas as pd
+
 st.title("Financial Transaction System")
-st.write("App is running successfully")
+
+if st.button("Generate Transactions"):
+    generate_data.generate_data()
+    st.success("Transactions generated successfully!")
+conn = sqlite3.connect("transactions.db")
+df = pd.read_sql("SELECT * FROM transactions ORDER BY date DESC LIMIT 10", conn)
+
+st.subheader("Recent Transactions")
+st.dataframe(df)
